f342046c061a844f2de17c3bca8203ac7ae1053c
Kevin-nn <thekevincaldon@gmail.com>
2024-11-20 12:50:05 -0500

    conectandome con el backed

---
+++ src/pages/Dashboard.jsx
@@ -0,0 +1,5 @@
+const Dashboard = () => {
+    return <h1>Bienvenido al Dashboard</h1>;
+};
+
+export default Dashboard;
---
+++ src/pages/LoginPage.jsx
@@ -0,0 +1,53 @@
+import { useState } from 'react';
+import axios from 'axios';
+
+function LoginPage() {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleLogin = async (e) => {
+    e.preventDefault();
+    try {
+      const response = await axios.post('http://localhost:4000/auth/login', {
+        email,
+        password,
+      });
+      console.log('Login successful:', response.data);
+    } catch (error) {
+      console.error('Error logging in:', error.response.data);
+    }
+  };
+
+  return (
+    <div className="container mt-5">
+      <h2>Login</h2>
+      <form onSubmit={handleLogin}>
+        <div className="mb-3">
+          <label htmlFor="email" className="form-label">Email</label>
+          <input
+            type="email"
+            className="form-control"
+            id="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            required
+          />
+        </div>
+        <div className="mb-3">
+          <label htmlFor="password" className="form-label">Password</label>
+          <input
+            type="password"
+            className="form-control"
+            id="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            required
+          />
+        </div>
+        <button type="submit" className="btn btn-primary">Login</button>
+      </form>
+    </div>
+  );
+}
+
+export default LoginPage;
